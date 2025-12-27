52d8c29bfac39a4875dcba878385b9879fb333a1
Adrian Mesa Sacasas <adrianmesasacasas@outlook.com>
2025-12-27 09:59:45 -0500

    work on it

--- cost_price_sheet_cu/__manifest__.py
+++ cost_price_sheet_cu/__manifest__.py
@@ -1,19 +1,19 @@
 {
     'name': 'Cost-Price Sheet CU',
     'summary' : 'Fichas de costo y precio',
-    'version': '19.0.0.1',
+    'version': '19.0.1.0.0',
     'author': 'Adrian Mesa Sacasas - Rulerhub',
-    'License' : '',
+    'License' : 'AGPL-3',
     'category': 'Accounting/Cost',
     'depends': [
-        'base', 'account', 'sale', 'purchase', 'stock', 'l10n_cu'
+        'base', 'mail', 'product', 'account', 'sale', 'purchase', 'stock', 'l10n_cu'
     ],
     'data': [
         "security/cost_price_sheet_security.xml",
         "security/ir.model.access.csv",
         "data/cost_price_sheet_sequence.xml",
         "views/cost_component_views.xml",
-        "views/cost_price_sheet_report.xml",
+        "report/cost_price_sheet_report.xml",
     ],
     'aplication': True
 }
--- cost_price_sheet_cu/models/cost_price_sheet.py
+++ cost_price_sheet_cu/models/cost_price_sheet.py
@@ -1,2 +1,14 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
+
+class CostPriceSheet(models.Model):
+    _name = "cost.price.sheet"
+    _description = "Ficha de costo-Precio (CU)"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
+    _order = "create_date desc"
+
+    name = fields.Char(string="Referencia", requiered=True, index=True, copy=False,
+                       default=lambda self: self.env["ir.sequence"].next_by_code("cost.price.sheet"))
+    product_id = fields.Many2one("product.product", string="Producto", required=True)
+    uom_id = fields.Many2one("uom.uom", string="Ud. de medida", related="product_id.uom_id", readonly=True)
+    company_id = fields.Many2one
